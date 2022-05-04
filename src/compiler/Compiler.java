50b0f715f0ce496f986b6fef044df11d23f9ecf4
Emad <emadaghajanzade@yahoo.com>
2022-05-05 00:02:50 +0430

    In this version, all the preliminary actions said in video have been added.

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -1,12 +1,29 @@
 package compiler;
-import org.antlr.runtime.CharStream;
+import gen.jythonLexer;
+import gen.jythonListener;
+import gen.jythonParser;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import java.awt.image.ImagingOpException;
 import java.io.IOException;
 
 public class Compiler {
     public static void main(String[] args) throws IOException{
+        CharStream stream = CharStreams.fromFileName("./sample/test.cl");
+        jythonLexer lexer = new jythonLexer(stream);
+        TokenStream tokens = new CommonTokenStream(lexer);
+        jythonParser parser = new jythonParser(tokens);
+        parser.setBuildParseTree(true);
+        ParseTree tree = parser.program();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        jythonListener listener = new CustomListener();
+
+
 
 
     }
